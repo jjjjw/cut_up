d640847344491435482173482b912b3e5549bd98
JJ Weber <jjweber@gmail.com>
2013-08-03 18:31:10 -0700

    Simple word tokenizer

--- test_tokenizer.py
+++ test_tokenizer.py
@@ -4,7 +4,6 @@ class TestMultiSub(TestCase):
     @classmethod
     def setUpClass(cls):
         cls.sample_sentence = "The sky above the port was the color of television, tuned to a dead channel."
-        cls.sample_text = "The sky above the port was the color of television, tuned to a dead channel. 'It's not like I'm using,' Case heard someone say, as he shouldered his way through the crowd around the door of the Chat. 'It's like my body's developed this massive drug deficiency.' It was a Sprawl voice and a Sprawl joke. The Chatsubo was a bar for professional expatriates; you could drink there for a week and never hear two words in Japanese. Ratz was tending bar, his prosthetic arm jerking monotonously as he filled a tray of glasses with draft Kirin. He saw Case and smiled, his teeth a webwork of East European steel and brown decay. Case found a place at the bar, between the unlikely tan on one of Lonny Zone's whores and the crisp naval uniform of a tall African whose cheekbones were ridged with precise rows of tribal scars. 'Wage was in here early, with two joeboys,' Ratz said, shoving a draft across the bar with his good hand. 'Maybe some business with you, Case?' Case shrugged. The girl to his right giggled and nudged him."
 
     def test_word_sub(self):
         """It should return a string with the word substitutions correctly made.
@@ -37,6 +36,21 @@ class TestMultiSub(TestCase):
         res = silly_replacements.sub(self.sample_sentence)
         self.assertEqual(res,
             "The above sky the port was the color of television , tuned to a dead channel!")
+
+
+class TestWordTokenize(TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.sample_sentence = "The sky above the port was the color of television, tuned to a dead channel."
+        cls.sample_text = "The sky above the port was the color of television, tuned to a dead channel. 'It's not like I'm using,' Case heard someone say, as he shouldered his way through the crowd around the door of the Chat. 'It's like my body's developed this massive drug deficiency.' It was a Sprawl voice and a Sprawl joke. The Chatsubo was a bar for professional expatriates; you could drink there for a week and never hear two words in Japanese. Ratz was tending bar, his prosthetic arm jerking monotonously as he filled a tray of glasses with draft Kirin. He saw Case and smiled, his teeth a webwork of East European steel and brown decay. Case found a place at the bar, between the unlikely tan on one of Lonny Zone's whores and the crisp naval uniform of a tall African whose cheekbones were ridged with precise rows of tribal scars. 'Wage was in here early, with two joeboys,' Ratz said, shoving a draft across the bar with his good hand. 'Maybe some business with you, Case?' Case shrugged. The girl to his right giggled and nudged him."
+
+    def test_get_words(self):
+        """It should return a list of words, ignoring punctuation.
+
+        """
+        from tokenizer import word_tokenize
+        res = word_tokenize(self.sample_sentence)
+        self.assertEqual(res, ['The', 'sky', 'above', 'the', 'port', 'was', 'the', 'color', 'of', 'television', 'tuned', 'to', 'a', 'dead', 'channel'])
 
 
 if __name__ == '__main__':
--- tokenizer.py
+++ tokenizer.py
@@ -1,57 +1,13 @@
 from multisub import MultiSub
 
-# Hygienic replacements
-QUOTATIONS = MultiSub({
-    r'^\"': r'``',
-    r'(``)': r' \1 ',
-    r'([ (\[{<])"': r'\1 ``',
+CLEAN_WORDS = MultiSub({
+    r'[^\w\s.]': None, # abbreviations currently unhandled
+    r'\.$': None, # sentence periods currently unhandled
 })
 
-PUNCTUATION = MultiSub({
-    r'([:,])([^\d])': r' \1 \2',
-    r'\.\.\.': r' ... ',
-    r'[;@#$%&]': r' \g<0> ',
-    r'([^\.])(\.)([\]\)}>"\']*)\s*$': r'\1 \2\3 ',
-    r'[?!]': r' \g<0> ',
-    r"([^'])' ": r"\1 '",
-})
-
-# List of contractions adapted from Robert MacIntyre's tokenizer.
-CONTRACTIONS = MultiSub({
-    r"(?i)\b(can)(not)\b": r' \1 \2 ',
-    r"(?i)\b(d)('ye)\b": r' \1 \2 ',
-    r"(?i)\b(gim)(me)\b": r' \1 \2 ',
-    r"(?i)\b(gon)(na)\b": r' \1 \2 ',
-    r"(?i)\b(got)(ta)\b": r' \1 \2 ',
-    r"(?i)\b(lem)(me)\b": r' \1 \2 ',
-    r"(?i)\b(mor)('n)\b": r' \1 \2 ',
-    r"(?i)\b(wan)(na) ": r' \1 \2 ',
-    r"(?i) ('t)(is)\b": r' \1 \2 ',
-    r"(?i) ('t)(was)\b": r' \1 \2 ',
-})
-
-def word_tokenize( text: "string"):
-    """The tokenizer creates a cleaned list of words and punctuation.
-    Adapted/copied from NLTK's (tokenizer)
-    [https://github.com/nltk/nltk/blob/master/nltk/tokenize/treebank.py]
-
-    Uses single pass multiple replace where this makes sense, following the (recipe)
-    [http://code.activestate.com/recipes/81330-single-pass-multiple-replace/]
-
-    (When NLTK works with python 3, the NLTK tokenizer module can be more or less
-    used as a drop in replacement, the key difference being the handling of sentences.)
+def word_tokenize(text: "string"):
+    """Dramatically simple word tokenizer, returns the list of words.
 
     """
-    text = _clean_quotations(text)
-    text = _clean_punctuation(text)
-    text = _clean_contractions(text)
+    text = CLEAN_WORDS.sub(text)
     return text.split()
-
-def _clean_quotations(text: "string"):
-    return QUOTATIONS.sub(text)
-
-def _clean_punctuation(text: "string"):
-    return PUNCTUATION.sub(text)
-
-def _clean_contractions(text: "string"):
-    return CONTRACTIONS.sub(text)
