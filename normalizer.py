e8aec3ef1c1c3145ce6b50f8a906c3674ebe05ad
JJ Weber <jjweber@gmail.com>
2013-08-08 00:03:42 -0700

    More sillyness

--- normalizer.py
+++ normalizer.py
@@ -12,22 +12,20 @@ def _punct_re() -> type(re_compile('')):
     return re_compile(r'([%s])' % re_escape(punctuation))
 
 
-def _steps() -> tuple:
+def _steps() -> list:
     """The normalization steps.
 
     """
     punct_re = _punct_re()
-    return (
+    return [
         lambda text: re_sub(punct_re, r' \1 ', text),  # Add whitespace
         lambda text: re_sub(r'\s+', ' ', text).strip(),  # Smooth superfluous whitespace
-    )
-
-
-STEPS = _steps()
+    ]
 
 
 def normalize_symbol_boundaries(text: str) -> str:
     """Given a text, inserts whitespace between words, commas, quotations, parens, etc.
 
     """
-    return reduce(lambda x, y: y(x), STEPS, text)
+    steps = _steps()
+    return reduce(lambda x, y: y(x), steps, text)
