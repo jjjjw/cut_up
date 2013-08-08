ac829e4b31575f03c0f53e82d5b7ccf46ab87fb1
JJ Weber <jjweber@gmail.com>
2013-08-07 19:54:41 -0700

    Functionalize

--- normalizer.py
+++ normalizer.py
@@ -1,31 +1,32 @@
-from re import compile
-from re import escape
-from re import sub
+from functools import reduce
+from re import compile as re_compile
+from re import escape as re_escape
+from re import sub as re_sub
 from string import punctuation
 
 
-def _pattern() -> type(compile('')):
-    """Compile the substitution regex pattern for use.
+def _punct_re() -> type(re_compile('')):
+    """Compile the punctuation substitution regex pattern for use.
 
     """
-    punct = escape(punctuation)
-    punct = r'([%s])' % punct
-
-    return compile(punct)
+    return re_compile(r'([%s])' % re_escape(punctuation))
 
 
-PUNCT = _pattern()
+def _steps() -> tuple:
+    """The normalization steps.
 
+    """
+    def add_whitespace(text: str) -> str:
+        return re_sub(_punct_re(), r' \1 ', text)
 
-def _add_whitespace(text: str) -> str:
-    return sub(PUNCT, r' \1 ', text)
+    def smooth_superfluous_whitespace(text: str) -> str:
+        return re_sub(r'\s+', ' ', text).strip()
 
-def _smooth_superfluous_whitespace(text: str) -> str:
-    return sub(r'\s+', ' ', text).strip()
+    return (add_whitespace, smooth_superfluous_whitespace)
 
 
 def normalize_symbol_boundaries(text: str) -> str:
     """Given a text, inserts whitespace between words, commas, quotations, parens, etc.
 
     """
-    return _smooth_superfluous_whitespace(_add_whitespace(text))
+    return reduce(lambda x, y: y(x), _steps(), text)
