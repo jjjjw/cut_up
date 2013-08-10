d08123bc875ddb250eea8bd7a313fd7e84e71623
JJ Weber <jjweber@gmail.com>
2013-08-10 15:13:17 -0700

    Randomize blocks of tokens rather than atomic tokens

--- cutup.py
+++ cutup.py
@@ -1,3 +1,4 @@
+from itertools import chain
 from normalizer import Normalizer
 from random import Random
 
@@ -8,11 +9,41 @@ def cut_up(text: str, seed: int = 1):
     rearrangement.
 
     """
+    # Init our tools
     normalizer = Normalizer()
     random = Random(seed)
+
+    # Normalize the corpus
     tokens = normalizer.normalize_symbol_boundaries(text).split()
-    random.shuffle(tokens)
+
+    # Segment the tokens and randomize the segments
+    blocks = _get_blocks(tokens, random)
+    random.shuffle(blocks)
+
+    # Reconstitute and denormalize the corpus
+    tokens = list(chain.from_iterable(blocks))
     res = " ".join(tokens)
     res = normalizer.denormalize_symbol_boundaries(res)
 
     return res
+
+
+def _get_blocks(tokens: list, random: Random):
+    """Chunk the tokens into random blocks.
+
+    """
+    tokens_len = len(tokens)
+    consume_tokens = list(tokens)
+    blocks = []
+
+    while len(consume_tokens) > 0:
+        i = random.randint(0, tokens_len)
+        try:
+            block = consume_tokens[:i]
+        except:
+            block = consume_tokens
+
+        consume_tokens = consume_tokens[len(block):]
+        blocks.append(block)
+
+    return blocks
--- test_cutup.py
+++ test_cutup.py
@@ -1,18 +1,31 @@
+from cutup import _get_blocks
 from cutup import cut_up
+from random import Random
 from unittest import TestCase
+
 
 class TestCutUp(TestCase):
     @classmethod
     def setUpClass(cls):
         cls.sample_sentence = "The sky above the port was the color of television, tuned to a dead channel."
-        cls.cut_up_sentence = ", was channel dead television The of a the the tuned to color sky. above port"
+        cls.cut_up_sentence = "The sky above the the color of television, tuned to a port was dead channel."
+        cls.sample_list = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i']
 
-    def test_normalizer(self):
+    def test_cut_up(self):
         """It should scramble a text.
 
         """
         res = cut_up(self.sample_sentence)
         self.assertEqual(res, self.cut_up_sentence)
+
+    def test_get_blocks(self):
+        """It should create discrete sublists.
+
+        """
+        random = Random(1)
+        res = _get_blocks(self.sample_list, random)
+        self.assertTrue(len(res) == 2)
+        self.assertRaises(ValueError, res[1].index, res[0][-1])
 
 
 if __name__ == '__main__':
